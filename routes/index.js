5d783499e99cd1420c35f816c25bb25b054d8240
fadil199 <achmadfadilla@yahoo.com>
2025-08-02 03:00:24 +0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const book = require("./book")
 
+router.get("/", (req, res) => {
+    return res.status(200).json({
+      status: true,
+      version: "1.0",
+      message: "Rest API sqlite"
+    });
+  });
+
 router.use("/book", book);
 
 module.exports = router;
